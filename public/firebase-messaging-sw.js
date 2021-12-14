3e9a23101c50ce1de00411caa275573f0ffb3b42
isaidakbar <smahmudxojaev@gmail.com>
2021-12-14 18:21:22 +0500

    modifies: 23

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,16 @@
 /* eslint-disable no-undef */
 importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js');
-var firebaseConfig = {
-  apiKey: "AIzaSyCx5Hw53Af-_uVHhMdv9D4tEeAGzSbuSMg",
-  authDomain: "elite-ceremony-300706.firebaseapp.com",
-  projectId: "elite-ceremony-300706",
-  storageBucket: "elite-ceremony-300706.appspot.com",
-  messagingSenderId: "372364494410",
-  appId: "1:372364494410:web:67e4e1e2c41522683a08b3",
-  measurementId: "G-9ESRF246F1"
-}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAOh043tDGUFBbpIuoYvOuQNWGMB5JisxQ",
+  authDomain: "delever-1745c.firebaseapp.com",
+  projectId: "delever-1745c",
+  storageBucket: "delever-1745c.appspot.com",
+  messagingSenderId: "733773630256",
+  appId: "1:733773630256:web:bdb52ec69e30e2d7bbd6f5",
+  measurementId: "G-H1SNXHT7ST"
+};
 firebase.initializeApp(firebaseConfig)
 firebase.messaging()
 const messaging  = firebase.messaging()
--- src/firebase.js
+++ src/firebase.js
@@ -3,22 +3,24 @@ import 'firebase/firebase-messaging'
 
 
 export default async function firebaseInitialize (){
-var firebaseConfig = {
-    apiKey: "AIzaSyCx5Hw53Af-_uVHhMdv9D4tEeAGzSbuSMg",
-    authDomain: "elite-ceremony-300706.firebaseapp.com",
-    projectId: "elite-ceremony-300706",
-    storageBucket: "elite-ceremony-300706.appspot.com",
-    messagingSenderId: "372364494410",
-    appId: "1:372364494410:web:67e4e1e2c41522683a08b3",
-    measurementId: "G-9ESRF246F1"
+  const firebaseConfig = {
+    apiKey: "AIzaSyAOh043tDGUFBbpIuoYvOuQNWGMB5JisxQ",
+    authDomain: "delever-1745c.firebaseapp.com",
+    projectId: "delever-1745c",
+    storageBucket: "delever-1745c.appspot.com",
+    messagingSenderId: "733773630256",
+    appId: "1:733773630256:web:bdb52ec69e30e2d7bbd6f5",
+    measurementId: "G-H1SNXHT7ST"
   };
 
 firebase.initializeApp(firebaseConfig)
- const messaging = firebase.messaging()
 
- messaging.getToken({ vapidKey: 'BBquQQmEpLBfdqpyYUtxbTYRi2BhvXv3RElkngZGoWpZTjBhpijKJqPt5TPumD-eKwWkIt2JDs2LAIvHhm8u8do' }).then((currentToken) => {
+ const messaging = firebase.messaging()
+ messaging.getToken({ vapidKey: 'BBpFfyoja5WnPrqAEZ_qUqm1MsykgcIk4Zh-fQG5fihvSijSxG1fdnlRrQgkWJRO8jcLtR4kandUfxtU_vJJTSI' }).then((currentToken) => {
   if (currentToken) {
     console.log(currentToken)
+    subscribeTokenToTopic(currentToken, 'd4b1658f-3271-4973-8591-98a82939a664');
+
   } else {
     console.log('No registration token available. Request permission to generate one.');
   }
@@ -42,4 +44,23 @@ messaging.onMessage(function(payload) {
       console.log(event);
     };
 });
+}
+
+function subscribeTokenToTopic(token, topic) {
+  fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
+    method: 'POST',
+    headers: new Headers({
+      Authorization: `key=AAAAqthRCzA:APA91bHRPrzPUDmgQ3L01qmYUcV5tYJMaA98X-6d8jQf2M5ocfU7NGqTSiStQ3r4XQezRAaxtbzpf85xs8SA2ddJquBRnBs-aMnLd3cweN7aDVBn3Q17XtctKrLp1bpHP3ThLP8dJ6xS`
+    })
+  })
+    .then((response) => {
+      if (response.status < 200 || response.status >= 400) {
+        console.log(response.status, response);
+      }
+      console.log(`"${topic}" is subscribed`);
+    })
+    .catch((error) => {
+      console.error(error.result);
+    });
+  return true;
 }
