fb4a60e70b82259afa533294e3034a69f62c094c
isaidakbar <smahmudxojaev@gmail.com>
2021-12-15 15:27:18 +0500

    modifies: firevase ur

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,25 +2,25 @@
 importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js');
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAOh043tDGUFBbpIuoYvOuQNWGMB5JisxQ",
-//   authDomain: "delever-1745c.firebaseapp.com",
-//   projectId: "delever-1745c",
-//   storageBucket: "delever-1745c.appspot.com",
-//   messagingSenderId: "733773630256",
-//   appId: "1:733773630256:web:bdb52ec69e30e2d7bbd6f5",
-//   measurementId: "G-H1SNXHT7ST"
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCx5Hw53Af-_uVHhMdv9D4tEeAGzSbuSMg",
-  authDomain: "elite-ceremony-300706.firebaseapp.com",
-  projectId: "elite-ceremony-300706",
-  storageBucket: "elite-ceremony-300706.appspot.com",
-  messagingSenderId: "372364494410",
-  appId: "1:372364494410:web:67e4e1e2c41522683a08b3",
-  measurementId: "G-9ESRF246F1"
+  apiKey: "AIzaSyAOh043tDGUFBbpIuoYvOuQNWGMB5JisxQ",
+  authDomain: "delever-1745c.firebaseapp.com",
+  projectId: "delever-1745c",
+  storageBucket: "delever-1745c.appspot.com",
+  messagingSenderId: "733773630256",
+  appId: "1:733773630256:web:bdb52ec69e30e2d7bbd6f5",
+  measurementId: "G-H1SNXHT7ST"
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCx5Hw53Af-_uVHhMdv9D4tEeAGzSbuSMg",
+//   authDomain: "elite-ceremony-300706.firebaseapp.com",
+//   projectId: "elite-ceremony-300706",
+//   storageBucket: "elite-ceremony-300706.appspot.com",
+//   messagingSenderId: "372364494410",
+//   appId: "1:372364494410:web:67e4e1e2c41522683a08b3",
+//   measurementId: "G-9ESRF246F1"
+// };
 
 firebase.initializeApp(firebaseConfig)
 firebase.messaging()
@@ -29,10 +29,37 @@ const messaging  = firebase.messaging()
 messaging.onBackgroundMessage((e) => {
     console.log("Public testing sevice worker", e)
     // Customize notification here
-    self.registration.showNotification(e.data.username,
+    self.registration.showNotification( 'Title in action',
       {
-        body: e.data.message,
-        image: 'https://i.insider.com/5f6096fc57b7da001ee11943?width=700',
-        icon: '',
+        body: 'You have a new order 🥳',
+        title: 'dasdasda',
+        icon: payload.notification.image,
+        image: payload.notification.image,
+        data: { url: e.data.order_id },
+        click_action: `https://app.delever.uz/orders/edit/${e.data.order_id}?status_id=986a0d09-7b4d-4ca9-8567-aa1c6d770505`,
+        actions: [{action: "open_url", title: "Read Now"}]
+
+      });
+      self.addEventListener('notificationclick', function(event) {
+        console.log('On notification click: ', event.notification.tag);
+        event.notification.close();
+      
+        // This looks to see if the current is already open and
+        // focuses if it is
+        event.waitUntil(
+          clients.matchAll({
+            type: "window"
+          })
+          .then(function(clientList) {
+            for (var i = 0; i < clientList.length; i++) {
+              var client = clientList[i];
+              if (client.url == '/' && 'focus' in client)
+                return client.focus();
+            }
+            if (clients.openWindow) {
+              return clients.openWindow('/');
+            }
+          })
+        );
       });
 });
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,107 @@
+import firebase from 'firebase/app'
+import 'firebase/firebase-messaging'
+
+
+export default async function firebaseInitialize (){
+
+  // firebase credentials
+  const firebaseConfig = {
+    apiKey: "AIzaSyAOh043tDGUFBbpIuoYvOuQNWGMB5JisxQ",
+    authDomain: "delever-1745c.firebaseapp.com",
+    projectId: "delever-1745c",
+    storageBucket: "delever-1745c.appspot.com",
+    messagingSenderId: "733773630256",
+    appId: "1:733773630256:web:bdb52ec69e30e2d7bbd6f5",
+    measurementId: "G-H1SNXHT7ST"
+  };
+
+  //firebase init
+firebase.initializeApp(firebaseConfig)
+
+  // firebase messaging instance
+ const messaging = firebase.messaging()
+
+ // get unique token by passing VAPIDKEY
+ messaging.getToken({ vapidKey: 'BKbuYZebt5FWpmE5q6CYM1OhIQEFe6_ByGu_A8m-zp3XvRT451Cbil-rsDnoMP3_EBOqeF9T2wT49yy2e--waWA' }).then((currentToken) => {
+  if (currentToken) {
+    console.log(currentToken)
+    subscribeTokenToTopic(currentToken, '1ecca1e7-5c34-4055-b685-df59e5021cbf');
+
+  } else {
+    console.log('No registration token available. Request permission to generate one.');
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+});
+
+
+
+// listen changes through Firebase on message listener
+
+messaging.onMessage(function(payload) {
+    console.log('Receiving foreground message');
+    console.log(payload)
+      const notificationOptions = {
+        body: 'You have a new order 🥳',
+        icon: payload.notification.image,
+        image: payload.notification.image
+      };
+
+    var notification = new Notification(payload.notification.title, notificationOptions);
+    notification.onclick = function(event) {
+      window.open(`https://app.delever.uz/orders/edit/${payload.data.order_id}?status_id=986a0d09-7b4d-4ca9-8567-aa1c6d770505`, '_blank')
+      notification.close();
+      console.log(event);
+    };
+});
+}
+
+// Subscribe to given token
+function subscribeTokenToTopic(token, topic) {
+  fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
+    method: 'POST',
+    headers: new Headers({
+      Authorization: `key=AAAAqthRCzA:APA91bFkYAdllJMdFMRn01SPrrSSEC9wi7AvTHE3Dfj3UQROduTITKogYqd6rkAgKZwkTdUTaV8txKFZXEyh9qKnCDOkx9VUWPLy-CU6uAc2ZkLmex2o-z4ao0sGcgIqhhPs32e1rbwX`
+    })
+  })
+    .then((response) => {
+      if (response.status < 200 || response.status >= 400) {
+        console.log(response.status, response.body);
+      }
+      console.log(`"${topic}" is subscribed`);
+      return response.body
+    }).then(res => console.log(res))
+    .catch((error) => {
+      console.error(error.body);
+    });
+  return true;
+}
+
+// Register navigation into Serviceworker
+
+navigator.serviceWorker.register('firebase-messaging-sw.js', {scope: "firebase-cloud-messaging-push-scope"})
+  .then((registration) => {
+    console.log(registration)
+  }).catch(err => {
+      console.log(err)
+  })
+
+
 // import firebase from 'firebase/app'
 // import 'firebase/firebase-messaging'
 
 
-// export default async function firebaseInitialize (){
+// export default async function firebaseInitialize (shipper_id = 'd4b1658f-3271-4973-8591-98a82939a664', t){
 
-//   // firebase credentials
+// console.log(shipper_id)  // firebase credentials
 //   const firebaseConfig = {
-//     apiKey: "AIzaSyAOh043tDGUFBbpIuoYvOuQNWGMB5JisxQ",
-//     authDomain: "delever-1745c.firebaseapp.com",
-//     projectId: "delever-1745c",
-//     storageBucket: "delever-1745c.appspot.com",
-//     messagingSenderId: "733773630256",
-//     appId: "1:733773630256:web:bdb52ec69e30e2d7bbd6f5",
-//     measurementId: "G-H1SNXHT7ST"
+//     apiKey: "AIzaSyCx5Hw53Af-_uVHhMdv9D4tEeAGzSbuSMg",
+//     authDomain: "elite-ceremony-300706.firebaseapp.com",
+//     projectId: "elite-ceremony-300706",
+//     storageBucket: "elite-ceremony-300706.appspot.com",
+//     messagingSenderId: "372364494410",
+//     appId: "1:372364494410:web:67e4e1e2c41522683a08b3",
+//     measurementId: "G-9ESRF246F1"
 //   };
 
 //   //firebase init
@@ -22,10 +111,12 @@
 //  const messaging = firebase.messaging()
 
 //  // get unique token by passing VAPIDKEY
-//  messaging.getToken({ vapidKey: 'BBpFfyoja5WnPrqAEZ_qUqm1MsykgcIk4Zh-fQG5fihvSijSxG1fdnlRrQgkWJRO8jcLtR4kandUfxtU_vJJTSI' }).then((currentToken) => {
+//  messaging.getToken({ vapidKey: 'BBquQQmEpLBfdqpyYUtxbTYRi2BhvXv3RElkngZGoWpZTjBhpijKJqPt5TPumD-eKwWkIt2JDs2LAIvHhm8u8do' }).then((currentToken) => {
 //   if (currentToken) {
 //     console.log(currentToken)
-//     subscribeTokenToTopic(currentToken, 'd4b1658f-3271-4973-8591-98a82939a664');
+//     t.notify = currentToken
+
+//     // subscribeTokenToTopic(currentToken, shipper_id);
 
 //   } else {
 //     console.log('No registration token available. Request permission to generate one.');
@@ -44,7 +135,8 @@
 //       const notificationTitle = payload.data.username;
 //       const notificationOptions = {
 //         body: payload.data.message,
-//         image: 'https://i.insider.com/5f6096fc57b7da001ee11943?width=700'
+//         image: 'https://i.insider.com/5f6096fc57b7da001ee11943?width=700',
+//         icon: ''
 //       };
 
 //     var notification = new Notification(notificationTitle, notificationOptions);
@@ -56,24 +148,24 @@
 // }
 
 // // Subscribe to given token
-// function subscribeTokenToTopic(token, topic) {
-//   fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
-//     method: 'POST',
-//     headers: new Headers({
-//       Authorization: `key=AAAAqthRCzA:APA91bHRPrzPUDmgQ3L01qmYUcV5tYJMaA98X-6d8jQf2M5ocfU7NGqTSiStQ3r4XQezRAaxtbzpf85xs8SA2ddJquBRnBs-aMnLd3cweN7aDVBn3Q17XtctKrLp1bpHP3ThLP8dJ6xS`
-//     })
-//   })
-//     .then((response) => {
-//       if (response.status < 200 || response.status >= 400) {
-//         console.log(response.status, response);
-//       }
-//       console.log(`"${topic}" is subscribed`);
-//     })
-//     .catch((error) => {
-//       console.error(error.result);
-//     });
-//   return true;
-// }
+// // function subscribeTokenToTopic(token, topic) {
+// //   fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
+// //     method: 'POST',
+// //     headers: new Headers({
+// //       Authorization: `key=AAAAqthRCzA:APA91bHRPrzPUDmgQ3L01qmYUcV5tYJMaA98X-6d8jQf2M5ocfU7NGqTSiStQ3r4XQezRAaxtbzpf85xs8SA2ddJquBRnBs-aMnLd3cweN7aDVBn3Q17XtctKrLp1bpHP3ThLP8dJ6xS`
+// //     })
+// //   })
+// //     .then((response) => {
+// //       if (response.status < 200 || response.status >= 400) {
+// //         console.log(response.status, response);
+// //       }
+// //       console.log(`"${topic}" is subscribed`);
+// //     })
+// //     .catch((error) => {
+// //       console.error(error.result);
+// //     });
+// //   return true;
+// // }
 
 // // Register navigation into Serviceworker
 
@@ -83,93 +175,3 @@
 //   }).catch(err => {
 //       console.log(err)
 //   })
-
-
-import firebase from 'firebase/app'
-import 'firebase/firebase-messaging'
-
-
-export default async function firebaseInitialize (shipper_id = 'd4b1658f-3271-4973-8591-98a82939a664', t){
-
-console.log(shipper_id)  // firebase credentials
-  const firebaseConfig = {
-    apiKey: "AIzaSyCx5Hw53Af-_uVHhMdv9D4tEeAGzSbuSMg",
-    authDomain: "elite-ceremony-300706.firebaseapp.com",
-    projectId: "elite-ceremony-300706",
-    storageBucket: "elite-ceremony-300706.appspot.com",
-    messagingSenderId: "372364494410",
-    appId: "1:372364494410:web:67e4e1e2c41522683a08b3",
-    measurementId: "G-9ESRF246F1"
-  };
-
-  //firebase init
-firebase.initializeApp(firebaseConfig)
-
-  // firebase messaging instance
- const messaging = firebase.messaging()
-
- // get unique token by passing VAPIDKEY
- messaging.getToken({ vapidKey: 'BBquQQmEpLBfdqpyYUtxbTYRi2BhvXv3RElkngZGoWpZTjBhpijKJqPt5TPumD-eKwWkIt2JDs2LAIvHhm8u8do' }).then((currentToken) => {
-  if (currentToken) {
-    console.log(currentToken)
-    t.notify = currentToken
-
-    // subscribeTokenToTopic(currentToken, shipper_id);
-
-  } else {
-    console.log('No registration token available. Request permission to generate one.');
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-});
-
-
-
-// listen changes through Firebase on message listener
-
-messaging.onMessage(function(payload) {
-    console.log('Receiving foreground message');
-    console.log(payload)
-      const notificationTitle = payload.data.username;
-      const notificationOptions = {
-        body: payload.data.message,
-        image: 'https://i.insider.com/5f6096fc57b7da001ee11943?width=700',
-        icon: ''
-      };
-
-    var notification = new Notification(notificationTitle, notificationOptions);
-    notification.onclick = function(event) {
-      notification.close();
-      console.log(event);
-    };
-});
-}
-
-// Subscribe to given token
-// function subscribeTokenToTopic(token, topic) {
-//   fetch(`https://iid.googleapis.com/iid/v1/${token}/rel/topics/${topic}`, {
-//     method: 'POST',
-//     headers: new Headers({
-//       Authorization: `key=AAAAqthRCzA:APA91bHRPrzPUDmgQ3L01qmYUcV5tYJMaA98X-6d8jQf2M5ocfU7NGqTSiStQ3r4XQezRAaxtbzpf85xs8SA2ddJquBRnBs-aMnLd3cweN7aDVBn3Q17XtctKrLp1bpHP3ThLP8dJ6xS`
-//     })
-//   })
-//     .then((response) => {
-//       if (response.status < 200 || response.status >= 400) {
-//         console.log(response.status, response);
-//       }
-//       console.log(`"${topic}" is subscribed`);
-//     })
-//     .catch((error) => {
-//       console.error(error.result);
-//     });
-//   return true;
-// }
-
-// Register navigation into Serviceworker
-
-navigator.serviceWorker.register('firebase-messaging-sw.js', {scope: "firebase-cloud-messaging-push-scope"})
-  .then((registration) => {
-    console.log(registration)
-  }).catch(err => {
-      console.log(err)
-  })
